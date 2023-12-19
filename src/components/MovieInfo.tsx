12ed247f5ab4d32ba278375dac0cc160a8a99fa1
Gabriel Zepeda <gabrielzepeda846@gmail.com>
2023-12-19 12:03:06 -0600

    Refactored MovieInfo component. Improved conditional rendering based on item Type. Added bootstrap classes to children elements.

--- src/components/MovieInfo.tsx
+++ src/components/MovieInfo.tsx
@@ -15,25 +15,31 @@ export type DataChildren = {
 };
 
 const MovieInfo: React.FC<MovieInfoProps> = ({ data }) => {
-
   return (
     <div className="container">
       <div className="row justify-content-between align-items-center">
-        {data.map((item, i) => {
-          const { Title, Year, Type, Poster } = item
-          const icon = Type === 'movie' ? <BiCameraMovie /> : <PiTelevisionSimple />
+        {data.map(({ Title, Year, Type, Poster }, i) => {
           return (
             <div key={i} className="col-4">
               <div className="card text-white bg-dark mb-3 m-2">
-                <img
-                  className="card-img-top"
-                  src={Poster}
-                  alt={Title}
-                />
+                <img className="card-img-top" src={Poster} alt={Title} />
                 <div className="card-body">
-                  <div className="card-title lead text-uppercase mb-1">{Title}</div>
-                  <p className="card-text mb-1">Released on: {Year}</p>
-                  <p className="card-text">{icon} {Type}</p>
+                  <div className="card-title lead text-uppercase h4 fs-6">
+                    {Title}
+                  </div>
+                  <p className="card-text fs-6 text-success h5">
+                    Released on: {Year}
+                  </p>
+                  <p className="card-text text-warning align-items-center d-flex justify-content-center">
+                    {Type === "movie" ? (
+                      <BiCameraMovie />
+                    ) : (
+                      <PiTelevisionSimple />
+                    )}
+                    <span className="mx-1 text-info text-capitalize fs-6">
+                      {Type}
+                    </span>
+                  </p>
                 </div>
               </div>
             </div>
