b8367ceb0fbed1f2f7fac1a905a2d8749498bb5b
Gabriel Zepeda <gabrielzepeda846@gmail.com>
2023-12-20 06:52:16 -0600

    Updated MovieInfo component with new icons and styling

--- src/components/MovieInfo.tsx
+++ src/components/MovieInfo.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { BiCameraMovie } from "react-icons/bi";
-import { PiTelevisionSimple } from "react-icons/pi";
+import { BiCameraMovie, BiTv } from "react-icons/bi";
 
 export type MovieInfoProps = {
   data: DataChildren[];
@@ -17,13 +16,20 @@ export type DataChildren = {
 const MovieInfo: React.FC<MovieInfoProps> = ({ data }) => {
   return (
     <div className="container">
-      <div className="row justify-content-between align-items-center">
+      <div className="row justify-content-between">
         {data.map(({ Title, Year, Type, Poster }, i) => {
           return (
-            <div key={i} className="col-4">
-              <div className="card text-white bg-dark mb-3 m-2">
-                <img className="card-img-top" src={Poster} alt={Title} />
-                <div className="card-body">
+            <div key={i} className="col-4 my-2">
+              <div
+                style={{ height: "100%", maxHeight: "750px" }}
+                className="card text-white bg-dark mb-3 m-2"
+              >
+                <img 
+                  style={{height: "80%"}}
+                  className="card-img-top" 
+                  src={Poster} 
+                  alt={Title} />
+                <div className="card-body d-flex flex-column justify-content-center">
                   <div className="card-title lead text-uppercase h4 fs-6">
                     {Title}
                   </div>
@@ -31,11 +37,7 @@ const MovieInfo: React.FC<MovieInfoProps> = ({ data }) => {
                     Released on: {Year}
                   </p>
                   <p className="card-text text-warning align-items-center d-flex justify-content-center">
-                    {Type === "movie" ? (
-                      <BiCameraMovie />
-                    ) : (
-                      <PiTelevisionSimple />
-                    )}
+                    {Type === "movie" ? <BiCameraMovie /> : <BiTv />}
                     <span className="mx-1 text-info text-capitalize fs-6">
                       {Type}
                     </span>
